794e1c08a11a658e6690794c5b07bf51119be5a6
Shubham singh <shubham1041singh@gmail.com>
2024-05-25 00:12:18 +0530

    ....

--- index.js
+++ index.js
@@ -4,8 +4,9 @@ const fs = require("fs");
 const myServer = http.createServer((req , res)=>{
     const log =`${Date.now()}:New Req Received\n`;
 fs.appendFile("log.txt", log , (err,data)=>{
-    res.end('hello from server again')  });
+    res.end('hello from server again');  
+});
 });
 
-myServer.listen(8003 , ()=> console.log("server started"));
+myServer.listen(8004 , ()=> console.log("server started"));
 
