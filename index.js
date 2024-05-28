7cae76dc9f6744d8b868a4f973bd0d524d10b4dd
Shubham singh <shubham1041singh@gmail.com>
2024-05-28 14:56:24 +0530

    ....

--- index.js
+++ index.js
@@ -1,15 +1,20 @@
 const http = require("http");
 const fs = require("fs");
+const url=require("url");
 
 const myServer = http.createServer((req , res)=>{
     if(req.url==="/favicon.ico") return res.end();
     const log =`${Date.now()}: ${req.url}New Req Received\n`;
+    const myUrl= url.parse(req.url,true);
+    console.log(myUrl);
 fs.appendFile("log.txt", log , (err,data)=>{
-    switch(req.url)
+    switch(myUrl.pathname)
     {
         case'/': res.end('home page'); 
         break;
-        case'/about':res.end("i am shubham singh");
+        case'/about':
+        const username=myUrl.query.myname
+        res.end("hi, ${username}");
         break;
         default:
             res.end("404 not found");
