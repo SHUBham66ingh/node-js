5d79c93fb254a3d4648a144b90903b393ce7329f
Shubham singh <shubham1041singh@gmail.com>
2024-05-28 15:59:28 +0530

    .....

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const myServer = http.createServer((req , res)=>{
     if(req.url==="/favicon.ico") return res.end();
     const log =`${Date.now()}:${req.method} ${req.url}New Req Received\n`;
     const myUrl= url.parse(req.url,true);
-    console.log(myUrl);
+  
 fs.appendFile("log.txt", log , (err,data)=>{
     switch(myUrl.pathname)
     {
