c5d27ae70196935c0c7f65aff57c8cead71c7200
Shubham singh <shubham1041singh@gmail.com>
2024-05-28 16:14:12 +0530

    .....

--- index.js
+++ index.js
@@ -1,25 +1,39 @@
 const http = require("http");
 const fs = require("fs");
 const url=require("url");
+const express=require("express")
 
-const myServer = http.createServer((req , res)=>{
-    if(req.url==="/favicon.ico") return res.end();
-    const log =`${Date.now()}:${req.method} ${req.url}New Req Received\n`;
-    const myUrl= url.parse(req.url,true);
+const app=express();
+
+app.get('/' , (req,res)=>{
+    return res.send("hello from home page");
+})
+
+app.get('/about' , (req,res)=>{
+    return res.send("hello from About page"); 
+})
+
+
+// let myServer = http.createServer((req , res)=>{
+//     if(req.url==="/favicon.ico") return res.end();
+//     const log =`${Date.now()}:${req.method} ${req.url}New Req Received\n`;
+//     const myUrl= url.parse(req.url,true);
   
-fs.appendFile("log.txt", log , (err,data)=>{
-    switch(myUrl.pathname)
-    {
-        case'/': res.end('home page'); 
-        break;
-        case'/about':
-        const username=myUrl.query.myname;
-        res.end(`hi,${username}`);
-        break;
-        default:
-            res.end("404 not found");
-    }
-});
-});
+// fs.appendFile("log.txt", log , (err,data)=>{
+//     switch(myUrl.pathname)
+//     {
+//         case'/': res.end('home page'); 
+//         break;
+//         case'/about':
+//         const username=myUrl.query.myname;
+//         res.end(`hi,${username}`);
+//         break;
+//         default:
+//             res.end("404 not found");
+//     }
+// });
+// });
+
+const myServer=http.createServer(app);
 myServer.listen(8004 , ()=> console.log("server started"));
 
