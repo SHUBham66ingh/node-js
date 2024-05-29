434e30c69a9b9492ee39f1eaed7805db023e7659
Shubham singh <shubham1041singh@gmail.com>
2024-05-29 11:34:06 +0530

    ......

--- index1.js
+++ index1.js
@@ -18,15 +18,20 @@ app.get('/users' , (req,res)=>{
    res.send(html);
 })
 
-app.route('/api/users/:id').get()
-
-
-app.get('/api/users/:id' , (req,res)=>{
+app.route('/api/users/:id').get((req,res)=>{
     const id= Number(req.params.id);
      const user=users.find(user=>user.id===id) ;
      return res.json(user);
 
+}).patch((req,res)=>{
+    //edit user with id
+  return  res.json({status:'pending'})
 })
+.delete((req,res)=>{
+ //delete user with id
+ return  res.json({status:'pending'})
+})
+
 
 app.post('/api/users' , (req,res)=>{
     //create new user
