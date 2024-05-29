39df526b2364036d24211fc4050d9b039a43b312
Shubham singh <shubham1041singh@gmail.com>
2024-05-29 10:44:45 +0530

    .......

--- index1.js
+++ index1.js
@@ -1,10 +1,11 @@
 const express=require('express');
 
+const users = require('./MOCK_DATA.json');
 
 const app=express();
 const PORT=8000;
 
 app.listen(PORT, ()=>{
     console.log(`server started at port : ${PORT}`);
-    
+
 })
