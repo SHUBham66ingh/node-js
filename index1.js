1f2f024ef84d79695ac81ed111a8c2b508e9a8a5
Shubham singh <shubham1041singh@gmail.com>
2024-05-31 20:55:51 +0530

    .......

--- index1.js
+++ index1.js
@@ -4,7 +4,7 @@ const {logReqRes} = require('./middlewares')
 const userRouter = require('./routes/user');
 const { default: mongoose } = require('mongoose');
 const app=express();
-const PORT=8009;
+const PORT=8007;
 
 mongoose.connect
 ('mongodb://127.0.0.1:27017/youtube-app-1')
@@ -17,8 +17,8 @@ app.use(express.urlencoded({extended:false}));
 app.use(logReqRes("log.txt"));
 
 
-app.use('/user' , userRouter);
-app.listen(PORT , ()=>console.log(`server started at PORT : ${PORT}`))
+app.use('/api/users' , userRouter);
+app.listen(PORT , ()=>console.log(`server started at PORT : ${PORT}`));
 
 
 
