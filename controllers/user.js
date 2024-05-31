cc7cfa8819e71256d4ce84156c6f329d515efb95
Shubham singh <shubham1041singh@gmail.com>
2024-05-31 19:29:47 +0530

    ........

--- controllers/user.js
+++ controllers/user.js
@@ -21,9 +21,34 @@ async function handleUpdateUserById(req,res){
     return  res.json({status:'Success'})
 }
 
+async function handleDeleteUserById(req,res){
+    await User.findByIdAndDelete(req.params.id)
+ return  res.json({status:'success'})
+}
+
+
+async function handleCreateUserById(req,res){
+    const body= req.body
+    if(!body||!body.first_name || !body.email||!body.gender|| !body.job_title|| !body.last_name )
+      {
+        return res.status(400).jason({msg:"ALL FIELDS ARE REQUIRED"})
+      }
+     const result= await User.create({
+        fisrtName:body.first_name ,
+        lastName:body.job_title,
+        email:body.email,
+        gender:body.gender,
+        jobTitle:body.job_title
+      });   
+      return res.status(201).json({msg:'success'});
+}
+
+
 
 module.exports={
   handleGetAllUsers,
   handleGetUserById,
   handleUpdateUserById,
+  handleDeleteUserById,
+  handleCreateUserById,
 }
--- routes/user.js
+++ routes/user.js
@@ -1,6 +1,6 @@
 const express = require('express');
-const {handleGetAllUsers,handleGetUsersById,handleUpdateUserById }= require('../controllers/users');
-const { getUserById } = require('../controllers/user');
+const {handleGetAllUsers,handleGetUsersById,handleUpdateUserById,handleDeleteUserById ,handleCreateUserById}= require('../controllers/users');
+const { getUserById, handleDeleteUserById } = require('../controllers/user');
 const router = express.Router();
 
 
@@ -12,28 +12,11 @@ router
 .route('/:id')
 .get(handleGetAllUsersById)
 .patch(handleUpdateUserById)
-.delete( async(req,res)=>{
-  await User.findByIdAndDelete(req.params.id)
- return  res.json({status:'success'})
-})
+.delete(handleDeleteUserById)
 
 
-router.post('/' , async(req,res)=>{
-    const body= req.body
-    if(!body||!body.first_name || !body.email||!body.gender|| !body.job_title|| !body.last_name )
-      {
-        return res.status(400).jason({msg:"ALL FIELDS ARE REQUIRED"})
-      }
-     const result= await User.create({
-        fisrtName:body.first_name ,
-        lastName:body.job_title,
-        email:body.email,
-        gender:body.gender,
-        jobTitle:body.job_title
-      });
-     
-      return res.status(201).json({msg:'success'});
-    })
+router.post('/' , handleCreateUserById)
+  
 
     module.exports = router
   
