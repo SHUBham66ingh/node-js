dfc044fe38c9a2da28db78b78341f1c10ba92c4d
Shubham singh <shubham1041singh@gmail.com>
2024-05-31 21:11:50 +0530

    ..............

--- routes/user.js
+++ routes/user.js
@@ -2,9 +2,7 @@ const express = require('express');
 const {handleGetAllUsers,handleGetUserById,handleUpdateUserById,handleDeleteUserById ,handleCreateUserById}= require('../controllers/user');
 const router = express.Router();
 
-
 router.route("/").get(handleGetAllUsers).post(handleCreateUserById)
-
 
 router
 .route('/:id')
