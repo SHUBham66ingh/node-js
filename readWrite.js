3a89b392241eaa0b1accc5ab9c92a0f934be4c68
Shubham singh <shubham1041singh@gmail.com>
2024-06-08 01:14:48 +0530

    ...

--- readWrite.js
+++ readWrite.js
@@ -1,15 +1,24 @@
 const fs = require('fs');
 
 
-fs.readFile('input.txt' ,(err, content)=>{
-       if(err)
-        {
-             return console.log(err)
-        }
-        else
-        {
-            console.log("read content is :"+ content)
-        }
-} )
+// fs.readFile('input.txt' ,(err, content)=>{
+//        if(err)
+//         {
+//              return console.log(err)
+//         }
+//         else
+//         {
+//             console.log("read content is :"+ content)
+//         }
+// } )
 
-console.log("hello from last")
+// console.log("hello from last")
+
+fs.writeFile('output.txt' , "hello students"  , err=>{
+    if(err)
+        {
+            return console.log(err)
+        }
+
+        console.log("written successfully");
+})
