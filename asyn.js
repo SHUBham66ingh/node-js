21372cc8482d1ddaad6ca8b9363a8b9df4a6d263
Shubham singh <shubham1041singh@gmail.com>
2024-06-08 00:19:22 +0530

    ....

---
+++ asyn.js
@@ -0,0 +1,10 @@
+console.log("hello students");
+
+function callback()
+{
+    console.log("hello again from students");
+}
+
+setTimeout(callback , 2000);
+
+console.log("hello from the final line");
